e9c48d97263704e92aa94adbb5c39c730076124c
Alizadeh <Alizadeh.Zahra@wirasystem.com>
2024-07-10 14:23:28 +0330

    third commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,5 @@
 import react, { useState } from "react";
-import { Button, Form, Container, Row, Col, InputGroup } from "react-bootstrap";
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
 
 function TodoList() {
   // create an array for tasks
@@ -15,13 +15,12 @@ function TodoList() {
     setNewTask(event.target.value);
   }
 
-  function addTask() {
-    {
-      if (newTask.trim() !== "") {
-        setTasks((prevState) => [...prevState, newTask]);
-        // setTasks([...tasks, newTask]);
-        setNewTask("");
-      }
+  function addTask(event) {
+    event.preventDefault();
+    if (newTask.trim() !== "") {
+      setTasks((prevState) => [...prevState, newTask]);
+      // setTasks([...tasks, newTask]);
+      setNewTask("");
     }
   }
 
@@ -54,69 +53,75 @@ function TodoList() {
   return (
     <>
       <Container className="to-do-list" fluid>
-        <div md={3}>
-          <Row>
-            <Col className="todo">
-              <h1>T O D O</h1>
+        <Row>
+          <Col className="HeaderRow" sm={{ span: 6, offset: 3 }}>
+            <h1>T O D O</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26">
+              <path
+                fill="#FFF"
+                fill-rule="evenodd"
+                d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"
+              />
+            </svg>
+          </Col>
+        </Row>
+
+        <Form>
+          <Row
+            className="align-items-center InputRow"
+            sm={{ span: 6, offset: 3 }}
+          >
+            <Col sm={5}>
+              <Form.Control
+                className="mb-2"
+                id="inlineFormInput"
+                type="text"
+                placeholder="Enter a task ..."
+                value={newTask}
+                onChange={handleInputChange}
+              />
             </Col>
-            <Col>
-              {" "}
-              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26">
-                <path
-                  fill="#FFF"
-                  fill-rule="evenodd"
-                  d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"
-                />
-              </svg>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <InputGroup className="mb-3">
-                <Form.Control
-                  placeholder="Enter a task ..."
-                  type="text"
-                  aria-label="Recipient's username"
-                  aria-describedby="basic-addon2"
-                  value={newTask}
-                  onChange={handleInputChange}
-                />
-                <Button variant="primary" id="button-addon2" onClick={addTask}>
-                  Add
-                </Button>
-              </InputGroup>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={{ span: 6, offset: 3 }}>
-              <ol>
-                {tasks.map((task, index) => (
-                  <li key={index}>
-                    <span className="text">{task}</span>
-                    <button
-                      className="delete-button"
-                      onClick={() => deleteTask(index)}
-                    >
-                      Delete
-                    </button>
-                    <button
-                      className="move-button"
-                      onClick={() => moveTaskUp(index)}
-                    >
-                      ⬆
-                    </button>
-                    <button
-                      className="move-button"
-                      onClick={() => moveTaskDown(index)}
-                    >
-                      ⬇
-                    </button>
-                  </li>
-                ))}
-              </ol>
+            <Col sm={1}>
+              <Button
+                type="submit"
+                className="mb-2 InputButton"
+                onClick={addTask}
+              >
+                Add
+              </Button>
             </Col>
           </Row>
-        </div>
+        </Form>
+
+        <Row>
+          <Col sm={{ span: 6, offset: 3 }}>
+            <ol>
+              {tasks.map((task, index) => (
+                <li key={index}>
+                  <span className="text">{task}</span>
+                  <button
+                    className="delete-button button"
+                    onClick={() => deleteTask(index)}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    className="move-button button"
+                    onClick={() => moveTaskUp(index)}
+                  >
+                    ⬆
+                  </button>
+                  <button
+                    className="move-button button"
+                    onClick={() => moveTaskDown(index)}
+                  >
+                    ⬇
+                  </button>
+                </li>
+              ))}
+            </ol>
+          </Col>
+        </Row>
       </Container>
     </>
   );
